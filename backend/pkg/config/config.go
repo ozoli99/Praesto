c6735a66296b38efcf4762262e3fc672e13b9b53
Zoltan Orosz <zoltan.orosz@leopoly.com>
2025-02-06 14:41:08 +0100

    Implement configuration loader module in config.go

---
+++ backend/pkg/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port        string
+	DatabaseURL string
+	AuthDomain  string
+	PaymentsKey string
+}
+
+func Load() *Config {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("No config file found, reading from env: %v", err)
+	}
+
+	return &Config{
+		Port: viper.GetString("PORT"),
+		DatabaseURL: viper.GetString("DATABASE_URL"),
+		AuthDomain: viper.GetString("AUTH_DOMAIN"),
+		PaymentsKey: viper.GetString("PAYMENTS_KEY"),
+	}
+}
