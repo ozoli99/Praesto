b29620a9285d0ca4d163a73545b3d4b7e3ed322f
Zoltan Orosz <zoltan.orosz@leopoly.com>
2025-02-06 15:01:15 +0100

    Add Base model

--- backend/pkg/appointment/appointment.go
+++ backend/pkg/appointment/appointment.go
@@ -3,10 +3,10 @@ package appointment
 import "time"
 
 type Appointment struct {
-	ID uint `json:"id"`
-	UserID uint `json:"user_id"`
-	ProviderID uint `json:"provider_id"`
-	Category string `json:"category"`
-	TimeSlot time.Time `json:"time_slot"`
-	Status string `json:"status"`
+	ID         uint      `json:"id"`
+	UserID     uint      `json:"user_id"`
+	ProviderID uint      `json:"provider_id"`
+	Category   string    `json:"category"`
+	TimeSlot   time.Time `json:"time_slot"`
+	Status     string    `json:"status"`
 }
