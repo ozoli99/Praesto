feb1053aaf64dca7cb79046cbae169c2c7c27286
Zoltan Orosz <zoltan.orosz@leopoly.com>
2025-02-06 15:04:07 +0100

    Add TODOs to integration package

--- backend/pkg/integration/auth.go
+++ backend/pkg/integration/auth.go
@@ -1,5 +1,7 @@
 package integration
 
+// Interacts with a third-party auth service to validate a token and retrieve user information
 func AuthenticateUser(token string) (bool, error) {
+	// TODO: Replace stub with an API call to auth provider
 	return true, nil
 }
--- backend/pkg/integration/payments.go
+++ backend/pkg/integration/payments.go
@@ -1,5 +1,7 @@
 package integration
 
+// Interacts with a payment provider to process a payment of a given amount in a specified currency
 func ProcessPayment(amount int, currency string) (string, error) {
+	// TODO: Replace stub with an API call to payment provider
 	return "payment_id_123", nil
 }
