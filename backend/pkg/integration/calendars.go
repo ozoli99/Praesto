c1185d4be39784f856496284f7142e383a933658
Zoltan Orosz <zoltan.orosz@leopoly.com>
2025-02-07 10:04:14 +0100

    Add calendar integration using Google Calendar API

--- backend/pkg/integration/calendars.go
+++ backend/pkg/integration/calendars.go
@@ -1,17 +1,69 @@
 package integration
 
 import (
+	"context"
 	"log"
+	"os"
+	"time"
 
 	"github.com/ozoli99/Praesto/pkg/appointment"
+
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
 )
 
+var calendarService *calendar.Service
+
+func init() {
+	context := context.Background()
+	credFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if credFile == "" {
+		log.Println("GOOGLE_APPLICATION_CREDENTIALS not set; calendar integration will be disabled")
+		return
+	}
+	service, err := calendar.NewService(context, option.WithCredentialsFile(credFile))
+	if err != nil {
+		log.Printf("Unable to create Google Calendar service: %v", err)
+		return
+	}
+	calendarService = service
+}
+
 func SyncAppointmentToCalendar(appointment *appointment.Appointment) {
-	// TODO: Integrate with Google Calendar, Outlook, etc.
-	log.Printf("Syncing appointment %d to calendar: %v - %v", appointment.ID, appointment.StartTime, appointment.EndTime)
+	if calendarService == nil {
+		log.Println("Calendar service not initialized")
+		return
+	}
+	calendarID := os.Getenv("GOOGLE_CALENDAR_ID")
+	if calendarID == "" {
+		log.Println("GOOGLE_CALENDAR_ID not set")
+		return
+	}
+
+	event := &calendar.Event{
+		Summary: "Appointment",
+		Description: "Service appointment",
+		Start: &calendar.EventDateTime{
+			DateTime: appointment.StartTime.Format(time.RFC3339),
+			TimeZone: "UTC",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: appointment.EndTime.Format(time.RFC3339),
+			TimeZone: "UTC",
+		},
+	}
+	createdEvent, err := calendarService.Events.Insert(calendarID, event).Do()
+	if err != nil {
+		log.Printf("Unable to create event: %v", err)
+		return
+	}
+	log.Printf("Event created: %s", createdEvent.HtmlLink)
 }
 
 func RemoveAppointmentFromCalendar(appointment *appointment.Appointment) {
-	// TODO: Remove event using the calendar API
-	log.Printf("Removing appointment %d from calendar", appointment.ID)
+	if calendarService == nil {
+		log.Println("Calendar service not initialized")
+		return
+	}
+	log.Printf("RemoveAppointmentFromCalendar: Not implemented because event ID is not stored for appointment %d", appointment.ID)
 }
