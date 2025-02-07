b9da02d053dd31e5e5bd7c24069ebb5eb116e154
Zoltán Orosz <45926346+ozoli99@users.noreply.github.com>
2025-02-07 09:23:14 +0000

    Add notification service integration using Twilio

--- backend/pkg/notifications/notifier.go
+++ backend/pkg/notifications/notifier.go
@@ -3,15 +3,65 @@ package notifications
 import (
 	"log"
 	"time"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/ozoli99/Praesto/pkg/appointment"
 )
 
+func sendSMS(to string, message string) error {
+	accountSID := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	fromPhone := os.Getenv("TWILIO_FROM_PHONE")
+	if accountSID == "" || authToken == "" || fromPhone == "" {
+		log.Println("Twilio configuration not set; cannot send SMS")
+		return nil
+	}
+
+	apiURL := "https://api.twilio.com/2010-04-01/Accounts/" + accountSID + "/Messages.json"
+	data := url.Values{}
+	data.Set("From", fromPhone)
+	data.Set("To", to)
+	data.Set("Body", message)
+
+	request, err := http.NewRequest("POST", apiURL, strings.NewReader(data.Encode()))
+	if err != nil {
+		return err
+	}
+	request.SetBasicAuth(accountSID, authToken)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 200 && response.StatusCode < 300 {
+		log.Println("SMS sent successfully")
+	} else {
+		log.Printf("Failed to send SMSl status: %s", response.Status)
+	}
+	return nil
+}
+
 func ScheduleReminder(appointment *appointment.Appointment) {
 	reminderTime := appointment.StartTime.Add(-1 * time.Hour)
-	log.Printf("Scheduling reminder for appointment %d at %v", appointment.ID, reminderTime)
-
-	// TODO: Use a job scheduler or message queue to send an email/SMS via SendGrid/Twilio
+	if time.Now().After(reminderTime) {
+		toPhone := os.Getenv("DUMMY_CUSTOMER_PHONE") // Retrieve a dummy phone number for testing or pull it from appointment data
+		if toPhone == "" {
+			toPhone = "+1234567890"
+		}
+		message := "Reminder: You have an appointment scheduled at " + appointment.StartTime.Format(time.RFC1123)
+		if er := sendSMS(toPhone, message); err != nil {
+			log.Printf("Error sending SMS reminder: %v", err)
+		}
+	} else {
+		log.Printf("Scheduled reminder for appointment %d at %v", appointment.ID, reminderTime)
+	}
 }
 
 func CancelReminder(appointment *appointment.Appointment) {
