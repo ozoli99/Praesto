f200976bfac9d66fffca63864aa32c642082665b
Zoltan Orosz <zoltan.orosz@leopoly.com>
2025-02-07 09:27:57 +0100

    Add dummy notification methods

--- backend/pkg/notifications/notifier.go
+++ backend/pkg/notifications/notifier.go
@@ -1,25 +1,20 @@
 package notifications
 
-import "log"
+import (
+	"log"
+	"time"
 
-type Notifier interface {
-	SendEmail(to, subject, body string) error
-	SendSMS(to, message string) error
+	"github.com/ozoli99/Praesto/pkg/appointment"
+)
+
+func ScheduleReminder(appointment *appointment.Appointment) {
+	reminderTime := appointment.StartTime.Add(-1 * time.Hour)
+	log.Printf("Scheduling reminder for appointment %d at %v", appointment.ID, reminderTime)
+
+	// TODO: Use a job scheduler or message queue to send an email/SMS via SendGrid/Twilio
 }
 
-// DummyNotifier is a placeholder implementation that logs notifications.
-type DummyNotifier struct{}
-
-func NewDummyNotifier() *DummyNotifier {
-	return &DummyNotifier{}
-}
-
-func (notifier *DummyNotifier) SendEmail(to, subject, body string) error {
-	log.Printf("Sending Email -> To: %s, Subject: %s, Body: %s", to, subject, body)
-	return nil
-}
-
-func (notifier *DummyNotifier) SendSMS(to, message string) error {
-	log.Printf("Sending SMS -> To: %s, Message: %s", to, message)
-	return nil
+func CancelReminder(appointment *appointment.Appointment) {
+	log.Printf("Canceling reminder for appointment %d", appointment.ID)
+	// TODO: Cancel the scheduled job if applicable
 }
