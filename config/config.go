68c7f6ad6a8a78a04ceb9a49924a2e1100429ca8
Zoltán Orosz <45926346+ozoli99@users.noreply.github.com>
2025-02-09 17:35:40 +0000

    Extend Configuration: add ClerkPublicKeyURL

--- config/config.go
+++ config/config.go
@@ -7,20 +7,21 @@ import (
 )
 
 type Configuration struct {
-	Port             string
-	DatabaseURL      string
-	AuthDomain       string
-	AuthAudience     string
-	AuthClientID     string
-	AuthClientSecret string
-	AuthCallbackURL  string
-	StripeSecretKey  string
-	PaymentAdapter   string
-	GoogleCredFile   string
-	GoogleCalendarID string
-	TwilioSID        string
-	TwilioAuthToken  string
-	TwilioFromPhone  string
+	Port              string
+	DatabaseURL       string
+	AuthDomain        string
+	AuthAudience      string
+	AuthClientID      string
+	AuthClientSecret  string
+	AuthCallbackURL   string
+	ClerkPublicKeyURL string
+	StripeSecretKey   string
+	PaymentAdapter    string
+	GoogleCredFile    string
+	GoogleCalendarID  string
+	TwilioSID         string
+	TwilioAuthToken   string
+	TwilioFromPhone   string
 }
 
 func Load(configFile string) *Configuration {
@@ -32,20 +33,21 @@ func Load(configFile string) *Configuration {
 	}
 
 	configuration := &Configuration{
-		Port:             viper.GetString("PORT"),
-		DatabaseURL:      viper.GetString("DATABASE_URL"),
-		AuthDomain:       viper.GetString("AUTH0_DOMAIN"),
-		AuthAudience:     viper.GetString("AUTH0_AUDIENCE"),
-		AuthClientID:     viper.GetString("AUTH0_CLIENT_ID"),
-		AuthClientSecret: viper.GetString("AUTH0_CLIENT_SECRET"),
-		AuthCallbackURL:  viper.GetString("AUTH0_CALLBACK_URL"),
-		StripeSecretKey:  viper.GetString("STRIPE_SECRET_KEY"),
-		PaymentAdapter:   viper.GetString("PAYMENT_ADAPTER"),
-		GoogleCredFile:   viper.GetString("GOOGLE_APPLICATION_CREDENTIALS"),
-		GoogleCalendarID: viper.GetString("GOOGLE_CALENDAR_ID"),
-		TwilioSID:        viper.GetString("TWILIO_ACCOUNT_SID"),
-		TwilioAuthToken:  viper.GetString("TWILIO_AUTH_TOKEN"),
-		TwilioFromPhone:  viper.GetString("TWILIO_FROM_PHONE"),
+		Port:              viper.GetString("Port"),
+		DatabaseURL:       viper.GetString("DatabaseURL"),
+		AuthDomain:        viper.GetString("AuthDomain"),
+		AuthAudience:      viper.GetString("AuthAudience"),
+		AuthClientID:      viper.GetString("AuthClientID"),
+		AuthClientSecret:  viper.GetString("AuthClientSecret"),
+		AuthCallbackURL:   viper.GetString("AuthCallbackURL"),
+		ClerkPublicKeyURL: viper.GetString("ClerkPublicKeyURL"),
+		StripeSecretKey:   viper.GetString("StripeSecretKey"),
+		PaymentAdapter:    viper.GetString("PaymentAdapter"),
+		GoogleCredFile:    viper.GetString("GoogleCredFile"),
+		GoogleCalendarID:  viper.GetString("GoogleCalendarID"),
+		TwilioSID:         viper.GetString("TwilioSID"),
+		TwilioAuthToken:   viper.GetString("TwilioAuthToken"),
+		TwilioFromPhone:   viper.GetString("TwilioFromPhone"),
 	}
 
 	log.Printf("Loaded DatabaseURL: %s", configuration.DatabaseURL)
